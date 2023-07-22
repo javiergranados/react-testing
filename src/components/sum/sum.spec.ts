e09ba9656c66583f64ab68b3214e1cb2011e93e0
Javier Granados <hola@javiergranados.dev>
2023-07-23 01:42:55 +0200

    Add eslint configuration

--- src/components/sum/sum.spec.ts
+++ src/components/sum/sum.spec.ts
@@ -1,7 +1,7 @@
 import { sum } from "./sum";
 
 it("suming 5 and 2 will return 7", () => {
-  const a: number = 5;
-  const b: number = 2;
+  const a = 5;
+  const b = 2;
   expect(sum(a, b)).toBe(7);
 });
