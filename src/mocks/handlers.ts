514107b18a48ed543909bccedbfa3c46c631c5df
Javier Granados <hola@javiergranados.dev>
2023-07-31 01:59:26 +0200

    Implement msw

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,29 @@
+import { Photo } from '@/common/types'
+import { rest } from 'msw'
+
+const defaultHandlers = [
+  rest.get('/api/photos', (req, res, ctx) => {
+    const name = req.url.searchParams.get('name') || 'Unknown'
+    return res(
+      ctx.json<Photo[]>([
+        {
+          id: 1,
+          thumbnailUrl: '/photo1.png',
+          title: name + ': Hello World',
+          favourite: false,
+        },
+      ])
+    )
+  }),
+
+  rest.post('/api/favourite', async (req, res, ctx) => {
+    const photo: Photo = await req.json()
+    return res(ctx.delay(200), ctx.json<Photo>({ ...photo, favourite: !photo.favourite }))
+  }),
+]
+
+const errorHandler = rest.get('/api/photos', (req, res, ctx) => {
+  return res(ctx.status(500), ctx.json<{ message: string }>({ message: 'Sorry Something happened!' }))
+})
+
+export { defaultHandlers, errorHandler }
