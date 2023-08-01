d4059bc259116d37e17c7cd845df40834c11a48a
Javier Granados <hola@javiergranados.dev>
2023-08-01 21:04:35 +0200

    Implement form component

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,16 +1,15 @@
 import { FormLayout } from '@/layouts/FormLayout'
 import ThemeRegistry from '@/layouts/ThemeRegistry'
-// import { Form as FormComponent } from '@/components/Form'
+import { Form as FormComponent } from '@/components/Form'
 
 export default function Form() {
   return (
     <FormLayout>
-      {/* <FormComponent
+      <FormComponent
         onSubmit={(values) => {
           console.log('Form Submitted', values)
         }}
-      /> */}
-      <h1>Form Page</h1>
+      />
     </FormLayout>
   )
 }
